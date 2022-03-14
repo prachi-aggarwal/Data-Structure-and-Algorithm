1fb51b207e8212b4ef85fef7ff68e4165b77e5ee
Prachi aggarwal <60839642+prachi-aggarwal@users.noreply.github.com>
2022-03-14 10:31:19 +0530

    Create LCA.java

---
+++ generic tree/LCA.java
@@ -0,0 +1,18 @@
+
+  public static int lca(Node node, int d1, int d2) {
+   ArrayList<Integer> a1=nodeToRootPath(node,d1);
+    ArrayList<Integer> a2=nodeToRootPath(node,d2);
+    int i=a1.size()-1;
+    int j=a2.size()-1;
+    int LCA=-1;
+    while(i>=0&&j>=0){
+        if(a1.get(i)!=a2.get(j)){
+            break;
+        }
+        LCA=a1.get(i);
+        i--;
+        j--;
+        
+    }
+    return LCA;
+  }
