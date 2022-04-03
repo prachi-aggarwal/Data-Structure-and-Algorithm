8d7dc2d53ae34c749d0c751bb1a947f1b602c8b6
Prachi aggarwal <60839642+prachi-aggarwal@users.noreply.github.com>
2022-04-03 12:12:32 +0530

    Update and rename unbounded knapsack to unbounded knapsack.java

--- DP/unbounded knapsack.java
+++ DP/unbounded knapsack.java
@@ -3,6 +3,7 @@ class Solution{
     static int knapSack(int N, int W, int val[], int wt[])
     {
         int [] dp=new int[W+1];
+        //i-->current target weight  and j-->weigth or option to explore
         for(int i=1;i<dp.length;i++){
         for(int j=0;j<wt.length;j++){
            if(i-wt[j]>=0){
