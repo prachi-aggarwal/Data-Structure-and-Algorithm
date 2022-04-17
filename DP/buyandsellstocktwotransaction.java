f15399b9d9e55127aa5c8af8be802b855f8a9f8a
Prachi aggarwal <60839642+prachi-aggarwal@users.noreply.github.com>
2022-04-17 11:08:32 +0530

    Update buyandsellstocktwotransaction.java

--- DP/buyandsellstocktwotransaction.java
+++ DP/buyandsellstocktwotransaction.java
@@ -5,6 +5,7 @@ class Solution {
         int [] left=new int[n];
         int [] right=new int[n];
         int buyingDay=prices[0];
+        //aj ya ajse phle phle becha ho
         for(int i=1;i<n;i++){
             int currprofit=prices[i]-buyingDay;
             if(left[i-1]>currprofit){
@@ -15,6 +16,7 @@ class Solution {
              left[i]=currprofit;
                 buyingDay=Math.min(buyingDay,prices[i]);
         }
+        //aj ya aj ke bad khareeda ho
         int sellingDay=prices[n-1];
         for(int i=n-2;i>=0;i--){
             int currprofit=sellingDay-prices[i];
@@ -26,7 +28,7 @@ class Solution {
              right[i]=currprofit;
                 sellingDay=Math.max(sellingDay,prices[i]);
         }
-        
+        //maximum profit of left[i] + right[i] for each day
         int maxprofit=0;
         for(int i=0;i<n;i++){
             int currprofit=left[i] +right[i];
