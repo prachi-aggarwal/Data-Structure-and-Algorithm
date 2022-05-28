b793f16e6e65dad8a46063f9a2bce0ed3ac9fd5a
Prachi aggarwal <60839642+prachi-aggarwal@users.noreply.github.com>
2022-05-28 20:10:51 +0530

    Time: 4 ms (41.02%), Space: 51.9 MB (53.47%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.java
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.java
@@ -1,36 +1,25 @@
 class Solution {
     public int longestMountain(int[] nums) {
-        int [] dp1=new int[nums.length];
-        int [] dp2=new int [nums.length];
-        Arrays.fill(dp1,1);
-        Arrays.fill(dp2,1);
-        int curr=0;
-        
+       //peak valley approach
+        int max=0;
+  
         for(int i=0;i<nums.length;i++){
-            // for(int j=0;j<i;j++){
-                if(i>0&&nums[i]>nums[i-1]){
-                  // curr++;
-                    dp1[i]=dp1[i-1]+1;
-                }
-              // dp1[i]=Math.max(dp1[i],curr);
-            
+          if(i>0&&i<nums.length-1&&nums[i]>nums[i-1]&&nums[i]>nums[i+1]){
+                int curr=1;
+            int left=i-1;
+              while(left>=0&&nums[left]<nums[left+1]){
+                  curr++;
+                  left--;
+              }
+              int right=i+1;
+              while(right<nums.length&&nums[right]<nums[right-1]){
+                 
+                  curr++;
+                   right++;
+              }
+              max=Math.max(curr,max);
+          }
         }
-        int count=0;
-           for(int i=nums.length-1;i>=0;i--){
-            // for(int j=nums.length-1;j>i;j--){
-                if(i<nums.length-1&&nums[i]>nums[i+1]){
-                                      dp2[i]=dp2[i+1]+1;
-                }
-             // dp2[i]=Math.max(dp2[i],count);
-        }
-        
-        int minele=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=dp1[i]+dp2[i]-1;
-            if(dp1[i]>1&&dp2[i]>1)
-            minele=Math.max(sum,minele);
-        }
-        
-        return minele;  
+         return max;   
     }
 }
