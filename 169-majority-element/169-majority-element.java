dba43b8172d48d11ce29e38e6403102912068e4b
Prachi aggarwal <60839642+prachi-aggarwal@users.noreply.github.com>
2022-07-04 02:41:48 +0530

    Time: 3 ms (62.91%), Space: 56.7 MB (24.85%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        //moore voting
+      int ele=0;
+     int   count =0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                ele=nums[i];
+             
+            }
+           if(ele==nums[i]){
+                count++;
+            }
+            else if(ele!=nums[i]){
+                count--;
+            }
+        }
+        
+        return ele;
+    }
+}
