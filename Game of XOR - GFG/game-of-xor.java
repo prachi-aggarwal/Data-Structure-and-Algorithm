97dd381b543004772c8ce200d75c39884fa01fc2
Prachi aggarwal <60839642+prachi-aggarwal@users.noreply.github.com>
2023-07-21 01:46:57 +0530

    Added solution - LeetHub

---
+++ Game of XOR - GFG/game-of-xor.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] A = new int[N];
+            
+            for(int i=0 ; i<N ; i++)
+                A[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.gameOfXor(N,A));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int gameOfXor(int N , int[] A) {
+       int res=0;
+        for(int i=0;i<N;i++){
+            int val=(i+1)*(N-i);
+            if((val&1)==1){
+                res=res^A[i];
+            }
+        }// code here
+        return res;
+    }
+};
